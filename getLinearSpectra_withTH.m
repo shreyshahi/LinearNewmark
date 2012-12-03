3afbfb14dc599aace8a2e2556439cb77bf71d4c3
shrey <shrey.shahi@gmail.com>
2012-12-03 13:46:38 -0800

    Added some comments in the script

--- getLinearSpectra_withTH.m
+++ getLinearSpectra_withTH.m
@@ -1,5 +1,28 @@
 function [S,H] = getLinearSpectra_withTH(T, z, ag, Dtg)
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%The script takes the following input :
+%
+%T   : Spectral acceleration period for the computation  
+%z   : Damping ratio (z = 0.005 for 5% damping)  
+%ag  : The acceleration time history of input ground-motion  
+%Dtg : The discreet time interval at which the input acceleration is recorded  
+%
+%The script returns the following:
+%
+%S  
+%|-S.d : Spectral displacement  
+%|-S.v : pseudo spectral velocity [S.d * ( 2 * pi / T )]  
+%|-S.a : pseudo spectral acceleration [S.d * ( 2 * pi / T ) ^ 2]  
+%
+%H  
+%|-H.d : Time history of the displacement response of the oscillator  
+%|-H.v : Time history of the velocity response of the oscillator  
+%|-H.a : Time history of the acceleration response of the oscillator  
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % Interpolate p=-ag*m (linearly)
 % ------------------------------
 minT = min(T);
